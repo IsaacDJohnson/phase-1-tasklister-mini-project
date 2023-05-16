d6982b6b860276f0da2da5fd4b9b238946f551bc
IsaacDJohnson <isaac.designws@gmail.com>
2023-05-16 10:22:56 -0500

    completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+document.addEventListener("DOMContentLoaded", function(){
+   
+    const taskForm = document.querySelector("#create-task-form")
+    const taskList = document.querySelector("#tasks")
+
+    taskForm.addEventListener("submit", function(e){
+        e.preventDefault()
+     
+        const newTask = document.querySelector("#new-task-description").value
+    
+        taskList.innerHTML += `<li> ${newTask} </li>`
+
+        taskForm.reset()
+   })
+})
